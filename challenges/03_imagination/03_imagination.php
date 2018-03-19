c6a95bbc323841ca7b2e810c39ed835388d44850
Strheilon <32923706+Strheilon@users.noreply.github.com>
2018-03-19 10:36:18 +0100

    Challenge n°3 exercice prof PHP

--- challenges/03_imagination/03_imagination.php
+++ challenges/03_imagination/03_imagination.php
@@ -1,1 +1,34 @@
-<?php  ?>
+<?php
+
+$argv;
+$quiche_lorraine = "quiche_lorraine";
+$tagliatelle_carbo = "tagliatelle_carbo";
+
+
+if ($argv[1] == $quiche_lorraine){
+    switch ($argv[2]) {
+        case 'info':
+            print "\nQuiche Lorraine : entrée alsacienne.\n";
+            break;
+        case 'part':
+            print "\nElle peut très bien être faite pour 4, pour 8 et même pour 1 seule personne.\n";
+            break;
+        case 'ingredients':
+            print "\nLardons, crème, oeuf, lait, farine, sel et fromage sont les principaux ingrédients d'une Quiche Lorraine.\n";
+            break;
+    }
+} elseif ($argv[1] == $tagliatelle_carbo){
+    switch ($argv[2]) {
+        case 'info':
+            print "\nTagliatelles à la Carbonnara : plat italien.\n";
+            break;
+        case 'part':
+            print "\nElles sont généralement servis pour 1 seule personne mais peut être cuisinées pour plusieurs personnes.\n";
+            break;
+        case 'ingredients':
+            print "\nLardons, crème et oeuf sont les principaux ingrédients des Tagliatelles Carbonnara.\n";
+            break;
+    }
+} else {
+    print "\nCe que vous demander n'est pas disponible.\n";
+}
